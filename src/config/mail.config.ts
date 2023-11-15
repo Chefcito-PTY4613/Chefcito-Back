87179d8d80b01767fc09fb34cfc9ac5efb812826
Javier <javier2000asr@gmail.com>
2023-11-15 19:07:38 -0300

    fear cambio de server

--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -1,28 +1,31 @@
-import nodemailer from "nodemailer";
+import { createTransport } from "nodemailer";
 import config from "./config";
 
-export const sendMail = async (mailUser:string, subject:string, html:string) => {
+export const sendMail = async (mailUser: string, subject: string, html: string) => {
   const configMail = {
-    host: "smtp-mail.outlook.com", // hostname
-    secureConnection: true, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
-    tls: {
-      ciphers: "SSLv3",
-    },
+    host: "smtp.office365.com",
+    secure: false, // Cambiado a false para el puerto 587
+    port: 587,
     auth: {
       user: config.MAIL.CORREO,
       pass: config.MAIL.PASS,
     },
   };
+
   const msg = {
     from: config.MAIL.CORREO,
     to: mailUser,
     subject: subject,
     html: Buffer.from(html, "utf-8").toString(),
   };
-  const transport = nodemailer.createTransport(configMail);
-  const info = await transport.sendMail(msg);
-  return info;
+
+  try {
+    const transport = createTransport(configMail);
+    const info = await transport.sendMail(msg);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
 };
 
 export default {
